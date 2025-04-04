2de96fae7e9a3351a3858f2bff903cc3ffc7bcd6
robertjmarlow <robertjmarlow@gmail.com>
2025-04-03 21:33:40 -0500

    Trimming trailing whitespace

--- src/js/bad-words-cache.ts
+++ src/js/bad-words-cache.ts
@@ -27,32 +27,32 @@ export class BadWordsCache {
       try {
         this.badWords = new Map();
         logger.info(`Getting bad words from "${process.env.badWordList}".`);
-    
+
         // get the bad word list csv
         const profanityCsvResponse = await axios.get(process.env.badWordList);
         if (profanityCsvResponse.status === 200) {
           const profanityCsv: string = profanityCsvResponse.data;
-    
+
           logger.info(`Read [${profanityCsv.length}] characters of bad words csv.`);
-    
+
           // turn the csv into an array of records
           const profanityRecords: any[] = parse(profanityCsv, {
             columns: true
           });
-    
+
           logger.info(`Read [${profanityRecords.length}] bad words records.`);
-    
+
           // turn the array of records into an array of BadWords
           for (let badWordIdx = 0; badWordIdx < profanityRecords.length; badWordIdx++) {
             const badWord: BadWord = BadWord.BadWordFactory(profanityRecords[badWordIdx]);
-    
+
             if (badWord !== undefined) {
               this.badWords.set(badWord.getText(), badWord);
             } else {
               logger.warn(`I had a problem reading ${profanityRecords[badWordIdx]}.`);
             }
           }
-    
+
           logger.info(`Added ${this.badWords.size} bad words.`);
         } else {
           logger.error(`Got a ${profanityCsvResponse.status} when reading bad words csv.`);
